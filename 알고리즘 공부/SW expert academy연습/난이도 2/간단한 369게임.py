b401939aafbdbd0bdc2d97595194685100abe2d9
dbswp <dbswp980427@gmail.com>
2023-02-12 14:30:46 +0900

    Update 백준 문제풀이

---
+++ 알고리즘 공부/SW expert academy연습/난이도 2/간단한 369게임.py
@@ -0,0 +1,9 @@
+n = int(input())
+for i in range(1,n+1):
+    count = 0
+    i = str(i)
+    clap = i.count('3')+i.count('6')+i.count('9')
+    if clap == 0:
+        print(i, end=' ')
+    else:
+        print("-"*clap, end=' ')
---
+++ 알고리즘 공부/백준 문제풀이 with js/입출력과 사칙연산/1000.js
@@ -0,0 +1,2 @@
+const inputData = require("fs").readFileSync("/dev/stdin").toString().split("");
+console.log(inputData[0] + inputData[1]);

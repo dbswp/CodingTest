c04a2ec77c6360932bf4415ea8ec66263d585562
dbswp <dbswp980427@gmail.com>
2023-02-14 15:09:17 +0900

    Update 프로그래머스js연습 -> 문자열 반복, 짝수와 홀수 구하기

---
+++ 알고리즘 공부/프로그래머스 js 연습/문자열 반복.js
@@ -0,0 +1,8 @@
+// function solution(my_string, n) {
+//   let arr = [];
+//   let answer = my_string.split("");
+//   for (let i = 0; i < my_string.length; i++) {
+//     arr += answer[i].repeat(n);
+//   }
+//   return arr;
+// }
--- 알고리즘 공부/프로그래머스 js 연습/짝수와 홀수개수.js
+++ 알고리즘 공부/프로그래머스 js 연습/짝수와 홀수개수.js
@@ -10,5 +10,6 @@
 //   return arr;
 // }
 
+// for i in arr 는 arr의 인덱스 값을, for i of arr 는 arr의 객체값을 가져온다.
 // // array의 값을 바로 ++로 증가 가능
 // // filter 함수사용해서 가능

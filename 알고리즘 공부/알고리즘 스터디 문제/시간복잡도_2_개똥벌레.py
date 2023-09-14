dd7b4fd8967462dea98d735525fbfa190ff74f69
dbswp <dbswp980427@gmail.com>
2023-09-14 18:35:29 +0900

    공간복잡도 - 개똥벌레

---
+++ 알고리즘 공부/알고리즘 스터디 문제/시간복잡도_2_개똥벌레.py
@@ -0,0 +1,14 @@
+import sys
+
+n,h = map(int, sys.stdin.readline().split())
+
+arr = [[0 for i in range(n)] for j in range(h)]
+
+for k in range(n):
+  huddle = list(int(input))
+  huddle[0]
+  if k % 2 == 0:
+    huddle[k] = arr[n][h]
+  
+print(arr)
+    

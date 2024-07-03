43fb791765debce8ef01e03551fe3c2200d04262
dbswp <dbswp980427@gmail.com>
2024-07-03 19:45:41 +0900

    코드잇 - 숫자 야구 게임 숫자 뽑기

---
+++ 알고리즘 공부/코드잇 py/숫자 야구게임/숫자3개뽑기.py
@@ -0,0 +1,20 @@
+from random import randint
+
+
+def generate_numbers():
+    numbers = []
+    
+    while len(numbers) < 3:
+        num = randint(0,9)
+        if num not in numbers:
+            numbers.append(num)
+        
+
+    # 여기에 코드를 작성하세요
+
+    print("0과 9 사이의 서로 다른 숫자 3개를 랜덤한 순서로 뽑았습니다.\n")
+    return numbers
+
+
+# 테스트 코드
+print(generate_numbers())
